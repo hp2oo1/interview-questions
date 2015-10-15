7e32675b35a262af8f86e45b049e85b2566c12df
hp2oo1 <huangpeng723@gmail.com>
2015-10-15 22:59:33 +0100

    Create solution.cpp

---
+++ loop-in-a-single-linked-list/solution.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node* next;
+    Node(int d) : data(d), next(nullptr) {}
+    Node* Append(int data) {
+        Node* new_node = new Node(data);
+        next = new_node;
+        return new_node;
+    }
+//
+friend void print( const Node* p );
+};
+
+void print( const Node* p ) {
+    int count = 0;
+    while( p!=0 ) {
+        cout<<p->data<<"->";
+        p = p->next;
+        ++count;
+        if( count>10 ) {
+            cout << "...\n";
+            return;
+        }
+    }
+    cout<<"nullptr"<<endl;
+}
+
+const Node* hasLoop( const Node* head ) {
+    const Node* p1(head);
+    const Node* p2(head);
+    while( p1!=nullptr && p2!=nullptr ) {
+        p1 = p1->next;
+        p2 = p2->next;
+        if( p2==nullptr )
+            break;
+        p2 = p2->next;
+        if( p1==p2 )
+            break;
+    }
+    if( p2==nullptr )
+        return p2; // no loop
+    // find first node starting the loop
+    // find pL = head + loop
+    const Node* pL(head);
+    while(1) {
+        pL = pL->next;
+        p1 = p1->next;
+        if( p1==p2 )
+            break;
+    }
+    // find p0 = head + x and p0+x == pL+x
+    const Node* p0(head);
+    while(1) {
+        p0 = p0->next;
+        pL = pL->next;
+        if( p0==pL )
+            break;
+    }
+    return p0;
+}
+
+int main() {
+    
+    Node *pHead = new Node(0);
+    pHead->Append(1)->Append(2)->Append(3);
+    // make a loop
+    pHead->next->next->next->next = pHead->next->next;
+    // check
+    print( pHead );
+    
+    // hasLoop?
+    const Node* p = hasLoop( pHead );
+    if( p==nullptr ) {
+        cout<<"no loop\n";
+    }
+    else {
+        cout<<"has loop starting at "<< p->data << endl;
+    }
+    
+}
